013b41f1474ce08b6c194a4067b3fc8373292b17
Volodymyr Shtyka <vladimirshtyka@gmail.com>
2024-03-06 19:13:56 +0000

    create Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <img src="../assets/images/avatar.jpeg" alt="Volodymyr Shtyka"/>
+            <h1>Volodymyr Shtyka</h1>
+            <p>I am a PHP web developer of sites and desktop applications.</p>
+            <p>I have more then 5 years experience in this field and more than 50 successfully completed orders of
+                various levels of complexity.</p>
+        </div>
+    )
+}
+
+export default Home;
