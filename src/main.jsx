4ee4a26d20fdcb1e6c356c9a10acee3162c1153c
Volodymyr Shtyka <vladimirshtyka@gmail.com>
2024-03-06 22:31:09 +0000

    material-ui installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2', // Change primary color
+        },
+        secondary: {
+            main: '#dc004e', // Change secondary color
+        },
+    },
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
 )
