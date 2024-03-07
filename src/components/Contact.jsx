3d44d5cb0f0771df3c3d8143b6bad12f5d2c691d
Volodymyr Shtyka <vladimirshtyka@gmail.com>
2024-03-07 00:17:23 +0000

    update contact using material-ui, change index.html title and icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import {Typography, Container} from '@mui/material';
+import {styled} from '@mui/system';
+
+const RootContainer = styled(Container)({
+    marginTop: '4rem',
+    textAlign: 'center',
+});
+
+const ContactLink = styled('a')({
+    marginLeft: '1rem',
+});
 
 function Contact() {
     return (
-        <div>
-            <h2>Contact Me</h2>
-            <p>Email: vladimirshtyka@gmail.com</p>
-            <p>LinkedIn: <a href="https://www.linkedin.com/in/vshtyka/">My LinkedIn Profile</a></p>
-            <p>GitHub: <a href="https://github.com/Volodymyr-Shtyka">My GitHub Profile</a></p>
-        </div>
+        <RootContainer>
+            <Typography variant="h2">Contact Me</Typography>
+            <Typography variant="body1">
+                Email:
+                <ContactLink href="mailto:vladimirshtyka@gmail.com">vladimirshtyka@gmail.com</ContactLink>
+            </Typography>
+            <Typography variant="body1">
+                LinkedIn:
+                <ContactLink href="https://www.linkedin.com/in/vshtyka/" target="_blank">My LinkedIn
+                    Profile</ContactLink>
+            </Typography>
+            <Typography variant="body1">
+                GitHub:
+                <ContactLink href="https://github.com/Volodymyr-Shtyka" target="_blank">My GitHub Profile</ContactLink>
+            </Typography>
+        </RootContainer>
     );
 }
 
