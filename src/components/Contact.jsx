944763dca08bfc39dd1568c8ef783f2a896ad562
Volodymyr Shtyka <vladimirshtyka@gmail.com>
2024-03-07 00:31:07 +0000

    update contact, add CV pdf file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Typography, Container} from '@mui/material';
+import {Typography, Container, Link, TextField, Button} from '@mui/material';
 import {styled} from '@mui/system';
 
 const RootContainer = styled(Container)({
@@ -7,8 +7,32 @@ const RootContainer = styled(Container)({
     textAlign: 'center',
 });
 
-const ContactLink = styled('a')({
+const ContactLink = styled(Link)({
     marginLeft: '1rem',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+});
+
+const FormContainer = styled(Container)({
+    marginTop: '2rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+});
+
+const FormField = styled(TextField)({
+    marginBottom: '1rem',
+    width: '100%',
+});
+
+const SubmitButton = styled(Button)({
+    marginTop: '1rem',
+    width: '100%',
+});
+
+const CVLink = styled(Link)({
+    textDecoration: 'none',
+    fontWeight: 'bold',
 });
 
 function Contact() {
@@ -28,6 +52,39 @@ function Contact() {
                 GitHub:
                 <ContactLink href="https://github.com/Volodymyr-Shtyka" target="_blank">My GitHub Profile</ContactLink>
             </Typography>
+            <Typography variant="body1">
+                CV:
+                <CVLink href="/public/Volodymyr_Shtyka_CV.pdf" target="_blank">Download PDF</CVLink>
+            </Typography>
+            <FormContainer>
+                <Typography variant="h3">Send me a message:</Typography>
+                <form>
+                    <FormField
+                        id="name"
+                        label="Your Name"
+                        variant="outlined"
+                        required
+                    />
+                    <FormField
+                        id="email"
+                        label="Your Email"
+                        variant="outlined"
+                        type="email"
+                        required
+                    />
+                    <FormField
+                        id="message"
+                        label="Your Message"
+                        variant="outlined"
+                        multiline
+                        rows={4}
+                        required
+                    />
+                    <SubmitButton variant="contained" color="primary" type="submit">
+                        Send
+                    </SubmitButton>
+                </form>
+            </FormContainer>
         </RootContainer>
     );
 }
