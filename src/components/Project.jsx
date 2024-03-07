74c126cee499dc1c220dd30d2c2019557aba35c9
Volodymyr Shtyka <vladimirshtyka@gmail.com>
2024-03-07 00:09:04 +0000

    update project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {Card, CardContent, CardMedia, Typography, Button} from "@mui/material";
+import {Typography, Link, Container} from "@mui/material";
+import {styled} from "@mui/system"; // Import styled from @mui/system
 
-function Project({title, deployedLink, githubLink, screenshot}) {
+const RootContainer = styled(Container)({
+    marginBottom: "1rem",
+    padding: "1rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+});
+
+const Screenshot = styled("img")({
+    maxWidth: "100%",
+    height: "auto",
+    marginBottom: "1rem",
+});
+
+function Project({title, deployedVersion, githubRepository, screenshot}) {
     return (
-        <Card>
-            <CardMedia
-                component="img"
-                height="200"
-                image={screenshot}
-                alt={title}
-            />
-            <CardContent>
-                <Typography variant="h3">{title}</Typography>
-                <Typography variant="body1">
-                    Deployed Version:{" "}
-                    <Link to={deployedLink} target="_blank" rel="noopener noreferrer">
-                        {deployedLink}
-                    </Link>
-                </Typography>
-                <Typography variant="body1">
-                    GitHub Repository:{" "}
-                    <Link to={githubLink} target="_blank" rel="noopener noreferrer">
-                        {githubLink}
-                    </Link>
-                </Typography>
-                <Button component={Link} to={githubLink} variant="contained" color="primary">
-                    View on GitHub
-                </Button>
-            </CardContent>
-        </Card>
+        <RootContainer>
+            <Typography variant="h3">{title}</Typography>
+            <Typography variant="body1" paragraph>
+                Deployed Version: <Link href={deployedVersion}>{deployedVersion}</Link>
+            </Typography>
+            <Typography variant="body1" paragraph>
+                GitHub Repository:{" "}
+                <Link href={githubRepository}>{githubRepository}</Link>
+            </Typography>
+            {screenshot && (
+                <Screenshot src={screenshot} alt={title}/>
+            )}
+        </RootContainer>
     );
 }
 
